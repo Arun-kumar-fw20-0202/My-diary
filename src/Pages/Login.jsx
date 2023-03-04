38b775f1bddd6df3f940bdc77caf24e79e1de442
Arun Kumar ( Katill_Boyy ) <arun07744@gmail.com>
2023-03-04 22:44:01 +0530

    Updates

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { handle_fetch_user } from "../Redux/Users/user.action";
 import "../Styles/register.css";
 // font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+
+const loginData = {
+  username: "",
+  password: "",
+};
 export const Login = () => {
+  const [data, setData] = useState(loginData);
+  const { username, password } = data;
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((store) => {
+    return {
+      user: store.UsersReducer.users,
+    };
+  }, shallowEqual);
+
+  const handleChange = (e) => {
+    let val = e.target.value;
+    setData({ ...data, [e.target.name]: val });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setData(loginData);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    dispatch(handle_fetch_user);
+  }, []);
+
   return (
     <div className="mainRegister">
       <div className="registerBx">
@@ -17,14 +49,26 @@ export const Login = () => {
         </div>
 
         {/*  */}
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="registerInputs">
             {/* <span>Username</span> */}
-            <input placeholder="Phone number, username , email" type="text" />
+            <input
+              placeholder="Phone number, username , email"
+              type="text"
+              onChange={(e) => handleChange(e)}
+              name="username"
+              value={username}
+            />
           </div>
           <div className="registerInputs">
             {/* <span>Mobile No:</span> */}
-            <input placeholder="password" type="password" />
+            <input
+              placeholder="password"
+              type="password"
+              onChange={(e) => handleChange(e)}
+              name="password"
+              value={password}
+            />
           </div>
           <div className="btnBx">
             <button>Sign Up</button>
